85e1eedeecc38fc37328f0e45694ee89d8bdf22b
fireland15 <fireland15@my.whitworth.edu>
2016-06-16 17:00:20 -0700

    Commit before branch

--- lindenmayer-system/lindenmayer-production.h
+++ lindenmayer-system/lindenmayer-production.h
@@ -9,11 +9,23 @@
 class Production {
 private:
 	ContextInspector m_contextInspector;
-	std::vector<LindenmayerSymbolBuilder> m_ResultBuilders;
+	std::vector<LindenmayerSymbolBuilder> m_resultBuilders;
 
 public:
 	Production(ContextInspector contextInspector, std::initializer_list<LindenmayerSymbolBuilder> )
 		: m_contextInspector(contextInspector) { }
 
+	bool CanReplace(unsigned int index, LindenmayerString& string) {
+		return m_contextInspector.TestContext(index, string);
+	}
 
+	LindenmayerString Produce(LindenmayerSymbol& symbol) {
+		LindenmayerString str;
+		for (auto builder : m_resultBuilders) {
+			for (float parameter : symbol.GetParameters()) {
+				builder.PassParameter("", parameter);
+			}
+			str.Add(builder.BuildSymbol());
+		}
+	}
 };
